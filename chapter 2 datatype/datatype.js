714d6aff44fb0f81cbbdaf851aa2cec24ba81364
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-07-15 12:52:41 +0500

    Add files via upload

---
+++ chapter 2 datatype/datatype.js
@@ -0,0 +1,19 @@
+// ....DATATYPES......
+// # datatype is the kind of data a value can hold,its tell the computer what kind of operation perform on data
+// string ,number, boolean , null, undefined, symbol
+
+const name = 'malika ';
+const age = 30;
+const isMarried=true;
+const rating = 4.5;
+const x=null;
+const y = undefined;
+let z;
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof x)
+console.log(typeof y)
+
+
+
+
