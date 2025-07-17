ebd7930a60c32ef068d4d527c4260c279b247398
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-07-18 00:30:14 +0500

    Add files via upload

---
+++ chapter 4 conditional statement/conditional statement.js
@@ -0,0 +1,45 @@
+// ...............................condtional statement............................
+// Conditional statements in JavaScript allow code to make decisions and execute different blocks 
+// of code based on whether certain conditions are true or false.
+
+
+// if statement
+let age=18;
+
+if (age>=18)
+{
+    console.log("you can vote")
+}
+if (age<18)
+{
+    console.log("you cannot vote")
+    }
+
+
+// if else
+    let age=18;
+
+    if (age>18)
+    {
+        console.log("you can vote")
+    }
+    else
+    {
+        console.log("you cannot vote")
+        }
+
+// else if 
+
+let age=20;
+
+if (age<18)
+{
+    console.log("junior")
+}
+else if(age>18)
+{
+    console.log("senior")
+    }
+    else{
+        console.log("middle")
+    }
