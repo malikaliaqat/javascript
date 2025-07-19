9ff2e41fd0f67acc93a3071d4fd26d863c965bc9
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-07-19 18:07:08 +0500

    Add files via upload
    for , while loop

---
+++ chapter 5 loop/for loop.js
@@ -0,0 +1,19 @@
+// CHAPTER 3:
+// ............................loops....................
+// In JavaScript, a loop is a control structure that allows 
+// a block of code to be executed repeatedly until a specified condition is met
+
+// for loop
+
+for (let i=0; i<=1000; i++)
+{
+    console.log("i love malika and dua")
+
+}
+sum=0;
+n=prompt("enter your number ")
+for (let i=1; i<=n ; i++)
+    sum=sum+i;
+{
+    console.log(sum)
+}
