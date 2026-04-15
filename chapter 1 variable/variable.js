eb050aedf49f8d0be4079156e09919f60c46a914
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-04-15 01:34:28 -0700

    Add files via upload

---
+++ chapter 1 variable/variable.js
@@ -0,0 +1,37 @@
+
+// // alert("heloo word")
+// // console.log("hello word")
+// // console.error ('This is an error')
+// // console.warn ('This is an warning')
+
+// // CHAPTER 1:
+
+// // .....................learning varable var,const,let................
+// //  let
+
+let a= 30;
+a = 31
+console.log(a);
+
+let sco;
+sco=23;
+sco=34;
+console.log(sco)
+
+// // const
+
+const age =30;
+console.log(age)
+
+const b = 30;
+console.log(b)
+
+// // var
+
+var num =30;
+num = 31;
+console.log(num)
+
+var no = 30;
+no =23;
+console.log(no)
